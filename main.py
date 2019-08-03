e079e6e41d5a712c53a849f5de07cab74c2f7a95
Izabela Melo <izabela.vmelo@gmail.com>
2019-08-03 16:27:53 -0300

    Calculating additional information

--- main.py
+++ main.py
@@ -15,9 +15,37 @@ def read_and_process_data(filename):
             name = line_splitted[3]
             lap = line_splitted[4]
             time = line_splitted[5]
-            data.setdefault(code, Pilot(name, code)).add_time(lap, time)
+            speed = line_splitted[6]
+            data.setdefault(code, Pilot(name, code)).add_lap(lap, time, speed)
 
     return data
+
+def render_the_best_lap_per_pilot(sorted_pilots):
+    print "\n===========================\nMelhor volta de cada piloto\n===========================\n"
+    print ",".join([
+        "Nome Piloto",
+        "Nº Volta",
+        "Tempo Volta"
+    ])
+    for pilot in sorted_pilots:
+        print "%s,%s,%s" % (pilot.name, pilot.better_lap, pilot.better_time)
+
+def render_the_best_time(sorted_pilots):
+    print "\n========================\nMelhor volta da corrida\n========================\n"
+    pilot_with_better_time = min(sorted_pilots,key=attrgetter('better_time'))
+    print "Nome Piloto: %s\nNº Volta: %s\nTempo Volta: %s" % (pilot_with_better_time.name, pilot_with_better_time.better_lap, pilot_with_better_time.better_time)
+
+def render_time_after_the_first(sorted_pilots):
+    print "\n================================\nTempo que chegou após o vencedor\n================================\n"
+    for v in sorted_pilots:
+        if(sorted_pilots.index(v) != 0):
+            print "%s: %s" % (v.name, v.get_time_after(sorted_pilots[0].total_time))
+
+def render_average_speed_per_pilot(sorted_pilots):
+    print "\n================================\nVelocidade média de cada piloto\n================================\n"
+    for v in sorted_pilots:
+        if(sorted_pilots.index(v) != 0):
+            print "%s: %s" % (v.name, v.get_average_speed())
 
 if __name__ == "__main__":
     data = read_and_process_data("log.txt")
@@ -34,3 +62,8 @@ if __name__ == "__main__":
     ])
     for v in sorted_pilots:
         print "%d,%s" % (sorted_pilots.index(v) + 1, v)
+
+    render_the_best_lap_per_pilot(sorted_pilots)
+    render_the_best_time(sorted_pilots)
+    render_average_speed_per_pilot(sorted_pilots)
+    render_time_after_the_first(sorted_pilots)
--- pilot.py
+++ pilot.py
@@ -5,16 +5,33 @@ class Pilot:
         self.name = name
         self.code = code
         self.times = {}
+        self.speeds = {}
         self.total_time = datetime.timedelta()
+        self.total_distance = 0.0
         self.completed_laps = 0
+        self.better_time = None
+        self.better_lap = None
 
-    def add_time(self, lap, time):
+    def add_lap(self, lap, time, speed):
         (m, s) = time.split(':')
         converted_time = datetime.timedelta(minutes=int(m), seconds=float(s))
         self.total_time += converted_time
+        self.total_distance += float(speed.replace(",", ".")) * converted_time.total_seconds()
         self.times[lap] = time
         if (self.completed_laps < int(lap)):
             self.completed_laps = int(lap)
+        if (not self.better_time or self.better_time.total_seconds() > converted_time.total_seconds()):
+            self.better_time = converted_time
+            self.better_lap = lap
+
+    def get_time_after(self, time):
+        if (self.completed_laps < 4):
+            return "-"
+
+        return str(datetime.timedelta(seconds=(self.total_time.total_seconds() - time.total_seconds())))
+
+    def get_average_speed(self):
+        return self.total_distance/self.total_time.total_seconds()
 
     def __cmp__(self, other):
         if self.completed_laps != other.completed_laps:
